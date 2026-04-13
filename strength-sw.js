680f9fcb48cc93a0e6b0ec8e9af27f3b5ca9322a
cskjak <dmlcks54@gmail.com>
2026-04-13 12:29:44 +0900

    chore: bump service worker cache to v3

--- strength-sw.js
+++ strength-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'strength-tracker-v2';
+const CACHE = 'strength-tracker-v3';
 const FILES = ['/strength_tracker.html','/strength-icon.svg','/strength-manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
